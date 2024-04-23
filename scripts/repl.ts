a6caee2882e208bdd6e58d682fe7e0ad4bc0c783
Mister Singh <mistersingh179@gmail.com>
2024-04-23 14:40:41 -0400

    form to manually add a contact.
    also fetching my-contacts with custom sql so we can join and only get when profiles are present.

--- scripts/repl.ts
+++ scripts/repl.ts
@@ -1,10 +1,5 @@
 import prisma from "../prismaClient";
-import redisClient from "@/lib/redisClient";
-import ProxyCurlQueue from "@/bull/queues/proxyCurlQueue";
-import { PersonExperience, Prisma, User } from "@prisma/client";
-import { ContactWithUserInfo } from "@/app/dashboard/prospects/page";
-import getEmailAndCompanyUrlProfiles from "@/services/getEmailAndCompanyUrlProfiles";
-import { distance } from "fastest-levenshtein";
+import { Contact } from "@prisma/client";
 
 // @ts-ignore
 prisma.$on("query", (e) => {
@@ -16,38 +11,14 @@ prisma.$on("query", (e) => {
 });
 
 (async () => {
-  console.log("hello world from repl");
-  const email = "rod@introhub.net";
-  const { emailToProfile, companyUrlToProfile } =
-    await getEmailAndCompanyUrlProfiles([email]);
-  console.dir(emailToProfile, { depth: 3 });
-  console.dir(companyUrlToProfile, { depth: 3 });
-
-  const emailDomainName = 'arrowheadcenter.nmsu.edu'; // email.match(/@(.*)/)?.[0]!;
-  const personalProfile = emailToProfile[email];
-
-  const bestPe = personalProfile.personExperiences.reduce<PersonExperience>((pe, cv) => {
-    const existingDistance = distance(
-      emailDomainName,
-      companyUrlToProfile[pe.companyLinkedInUrl].website!,
-    );
-    const newDistance = distance(
-      emailDomainName,
-      companyUrlToProfile[cv.companyLinkedInUrl].website!,
-    );
-    if (newDistance < existingDistance) {
-      return cv;
-    } else {
-      return pe;
-    }
-  }, personalProfile.personExperiences[0]);
-
-  personalProfile.personExperiences.map((pe) => {
-    const cp = companyUrlToProfile[pe.companyLinkedInUrl];
-    distance(cp.website!, emailDomainName!);
+  const contacts: Contact[] = await prisma.contact.findMany({
+    where: {
+      user: {
+        email: "sandeep@brandweaver.ai" || "",
+      },
+    },
   });
-
-  console.log("bestPe", bestPe);
+  console.log(contacts);
 })();
 
 export {};
