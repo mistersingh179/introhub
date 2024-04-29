3d32101d366080b6b236f4b690f975d6ea9a0c9d
Mister Singh <mistersingh179@gmail.com>
2024-04-29 13:11:20 -0400

    updating account access token & user profile stuff upon login

--- scripts/repl.ts
+++ scripts/repl.ts
@@ -1,6 +1,7 @@
 import prisma from "../prismaClient";
 import { Contact } from "@prisma/client";
 import getGmailObject from "@/services/helpers/getGmailObject";
+import { google } from "googleapis";
 
 // @ts-ignore
 prisma.$on("query", (e) => {
@@ -12,22 +13,41 @@ prisma.$on("query", (e) => {
 });
 
 (async () => {
-  console.log("Hello world");
-  const account = await prisma.account.findFirstOrThrow({
+  console.log("Hello world !!!");
+
+  const email = "mistersingh179@gmail.com";
+
+  let account = await prisma.account.findFirstOrThrow({
     where: {
       user: {
-        name: {
-          contains: "sandeep",
-          mode: "insensitive",
-        },
+        email,
       },
     },
   });
+  console.log(account);
+
   const gmail = await getGmailObject(account);
-  const profile = await gmail.users.getProfile({
+  const { data: profileData } = await gmail.users.getProfile({
     userId: "me",
   });
-  console.log("profile ", profile);
+  console.log("profile: ", profileData);
+
+  account = await prisma.account.findFirstOrThrow({
+    where: {
+      user: {
+        email,
+      },
+    },
+  });
+
+  const at = account.access_token as string;
+
+  const oauth2Client = new google.auth.OAuth2();
+  oauth2Client.setCredentials({
+    access_token: at,
+  });
+  const tokenInfo = await oauth2Client.getTokenInfo(at);
+  console.log("tokenInfo: ", tokenInfo);
 })();
 
 export {};
