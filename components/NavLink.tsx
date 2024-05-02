e754446b1e1f1cdcb9e704d6de6535e374e9b5b3
Mister Singh <mistersingh179@gmail.com>
2024-05-02 17:45:06 -0400

    navigation is now mobile friendly
    showing sheet or nav
    moved navitems out so they can be reused
    they wrap links in sheetclose when used inside a sheet

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,24 +1,31 @@
 "use client";
 
 import Link from "next/link";
-import {usePathname} from "next/navigation";
+import { usePathname } from "next/navigation";
+import { SheetClose } from "@/components/ui/sheet";
+import React from "react";
 
 type NavLinkProps = {
   name: string;
   url: string;
   className?: string;
+  inSheet?: boolean;
 };
 const NavLink = (props: NavLinkProps) => {
   const pathName = usePathname();
-  const { name, url, className } = props;
+  const { name, url, className, inSheet } = props;
+  const WrapperComponent = inSheet ? SheetClose : React.Fragment;
+  const wrapperProps = inSheet ? { asChild: true } : {};
   return (
-    <Link
-      className={`hover:underline ${pathName === url ? "font-semibold" : "auto"} ${className}`}
-      href={url}
-    >
-      {name}
-    </Link>
+    <WrapperComponent {...wrapperProps}>
+      <Link
+        className={`hover:underline ${pathName === url ? "font-semibold" : "auto"} ${className}`}
+        href={url}
+      >
+        {name}
+      </Link>
+    </WrapperComponent>
   );
 };
 
-export default NavLink
+export default NavLink;
