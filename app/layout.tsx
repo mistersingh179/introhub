d115da404b7d38604df52e0437abd9b672c392db
Mister Singh <mistersingh179@gmail.com>
2024-05-03 14:16:21 -0400

    added viewport export

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import type { Viewport } from 'next'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,15 @@ export const metadata: Metadata = {
   title: "IntroHub",
   description: "This is where magic happens.",
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  // Also supported by less commonly used
+  // interactiveWidget: 'resizes-visual',
+}
 
 export default function RootLayout({
   children,
