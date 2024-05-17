0754aa71bf8c01c7a48ca1d8f7f880c5998c9459
Mister Singh <mistersingh179@gmail.com>
2024-05-17 11:24:43 -0400

    refactored to use a service which gives prospects based on filters.
    on prospects index page using this service.

--- app/dashboard/prospects/page.tsx
+++ app/dashboard/prospects/page.tsx
@@ -17,8 +17,12 @@ import {
 import { Button } from "@/components/ui/button";
 import { ChevronsUpDown } from "lucide-react";
 import getUniqueValuesWithOrderPreserved from "@/services/getUniqueValuesWithOrderPreserved";
+import getProspectsBasedOnFilters, {
+  PaginatedValues,
+  SelectedFilterValues,
+} from "@/services/getProspectsBasedOnFilters";
 
-type ProspectsSearchParams = {
+export type ProspectsSearchParams = {
   query?: string;
   selectedCities?: string | string[];
   selectedStates?: string | string[];
@@ -51,19 +55,9 @@ const getValueAsArray = (
   }
 };
 
-type GetSelectedFilterValues = (
+export type GetSelectedFilterValues = (
   searchParams: ProspectsSearchParams | undefined,
-) => {
-  selectedCities: string[] | undefined;
-  selectedStates: string[] | undefined;
-  selectedJobTitles: string[] | undefined;
-  selectedIndustries: string[] | undefined;
-  selectedCategories: string[] | undefined;
-  selectedUserEmails: string[] | undefined;
-  selectedEmail: string | undefined;
-  selectedWebsite: string | undefined;
-  createdAfter: string | undefined;
-};
+) => SelectedFilterValues;
 
 const getSelectedFilterValues: GetSelectedFilterValues = (searchParams) => {
   const selectedCities = getValueAsArray(searchParams?.selectedCities);
@@ -94,11 +88,7 @@ const getSelectedFilterValues: GetSelectedFilterValues = (searchParams) => {
 
 type GetPaginationValues = (
   searchParams: ProspectsSearchParams | undefined,
-) => {
-  currentPage: number;
-  itemsPerPage: number;
-  recordsToSkip: number;
-};
+) => PaginatedValues;
 
 const getPaginationValues: GetPaginationValues = (searchParams) => {
   const currentPage = Number(searchParams?.page) || 1;
@@ -243,77 +233,9 @@ export default async function Prospects({
   const { cities, states, jobTitles, industries, categories, userEmails } =
     await getAllFilterValues(user);
 
-  const { recordsToSkip, currentPage, itemsPerPage } =
-    getPaginationValues(searchParams);
-
-  const {
-    selectedCities,
-    selectedStates,
-    selectedJobTitles,
-    selectedEmail,
-    selectedWebsite,
-    selectedIndustries,
-    selectedCategories,
-    selectedUserEmails,
-    createdAfter,
-  } = getSelectedFilterValues(searchParams);
-
-  const cityFilterSql = selectedCities
-    ? Prisma.sql`and PP.city in (${Prisma.join(selectedCities)})`
-    : Prisma.sql``;
-
-  const stateFilterSql = selectedStates
-    ? Prisma.sql`and PP.state in (${Prisma.join(selectedStates)})`
-    : Prisma.sql``;
-
-  const jobTitleFilterSql = selectedJobTitles
-    ? Prisma.sql`and PE."jobTitle" ~* ${"\\y(" + selectedJobTitles.join("|") + ")\\y"}`
-    : Prisma.sql``;
-
-  const industryFilterSql = selectedIndustries
-    ? Prisma.sql`and CP."industry" in (${Prisma.join(selectedIndustries)})`
-    : Prisma.sql``;
-
-  const categoriesFilterSql = selectedCategories
-    ? Prisma.sql`and CAT."name" in (${Prisma.join(selectedCategories)})`
-    : Prisma.sql``;
-
-  const userEmailsFilterSql = selectedUserEmails
-    ? Prisma.sql`and U."email" in (${Prisma.join(selectedUserEmails)})`
-    : Prisma.sql``;
-
-  const emailFilterSql = selectedEmail
-    ? Prisma.sql`and C.email ilike ${"%" + selectedEmail + "%"}`
-    : Prisma.sql``;
-
-  const websiteFilterSql = selectedWebsite
-    ? Prisma.sql`and C.email ilike ${"%" + selectedWebsite + "%"}`
-    : Prisma.sql``;
-
-  const createdAfterFilterSql = createdAfter
-    ? Prisma.sql`and C."createdAt" >= ${new Date(createdAfter)}`
-    : Prisma.sql``;
-
-  const sql = Prisma.sql`
-      select distinct on (C.email) C.*
-      from "Contact" C
-               inner join public."User" U on U.id = C."userId"
-               inner join public."PersonProfile" PP on C.email = PP.email and PP."linkedInUrl" is not null
-               inner join public."PersonExperience" PE on PP.id = PE."personProfileId"
-               inner join public."CompanyProfile" CP on CP."linkedInUrl" = PE."companyLinkedInUrl"
-               left join public."CompanyProfileCategory" CPC on CP.id = CPC."companyProfileId"
-               left join public."Category" CAT on CPC."categoryId" = CAT.id
-      where 1 = 1 ${cityFilterSql} ${stateFilterSql} ${jobTitleFilterSql} ${emailFilterSql} \
-            ${websiteFilterSql} ${industryFilterSql} ${categoriesFilterSql} ${userEmailsFilterSql} \
-            ${createdAfterFilterSql}
-        and C."userId" != ${user.id}
-      order by email ASC, "receivedCount" DESC
-      offset ${recordsToSkip} limit ${itemsPerPage};
-  `;
-
-  console.log(sql.text, sql.values);
-
-  const prospects = await prisma.$queryRaw<Contact[]>(sql);
+  const paginationValues: PaginatedValues = getPaginationValues(searchParams);
+  const filters: SelectedFilterValues = getSelectedFilterValues(searchParams);
+  const prospects = await getProspectsBasedOnFilters(filters, paginationValues, user);
 
   const userIds = [...new Set(prospects.map((p) => p.userId))];
   const users = await prisma.user.findMany({
---
+++ services/getProspectsBasedOnFilters.ts
@@ -0,0 +1,122 @@
+import { Contact, Prisma, User } from "@prisma/client";
+import prisma from "@/prismaClient";
+
+export type PaginatedValues = {
+  currentPage: number;
+  itemsPerPage: number;
+  recordsToSkip: number;
+}
+
+export type SelectedFilterValues = {
+  selectedCities?: string[] | undefined;
+  selectedStates?: string[] | undefined;
+  selectedJobTitles?: string[] | undefined;
+  selectedIndustries?: string[] | undefined;
+  selectedCategories?: string[] | undefined;
+  selectedUserEmails?: string[] | undefined;
+  selectedEmail?: string | undefined;
+  selectedWebsite?: string | undefined;
+  createdAfter?: string | undefined;
+}
+
+const getProspectsBasedOnFilters = async (
+  filters: SelectedFilterValues,
+  paginatedValues: PaginatedValues,
+  user: User,
+): Promise<Contact[]> => {
+  const {
+    selectedCities,
+    selectedStates,
+    selectedJobTitles,
+    selectedEmail,
+    selectedWebsite,
+    selectedIndustries,
+    selectedCategories,
+    selectedUserEmails,
+    createdAfter,
+  } = filters;
+
+  const { recordsToSkip, currentPage, itemsPerPage } = paginatedValues;
+
+  const cityFilterSql = selectedCities
+    ? Prisma.sql`and PP.city in (${Prisma.join(selectedCities)})`
+    : Prisma.sql``;
+
+  const stateFilterSql = selectedStates
+    ? Prisma.sql`and PP.state in (${Prisma.join(selectedStates)})`
+    : Prisma.sql``;
+
+  const jobTitleFilterSql = selectedJobTitles
+    ? Prisma.sql`and PE."jobTitle" ~* ${"\\y(" + selectedJobTitles.join("|") + ")\\y"}`
+    : Prisma.sql``;
+
+  const industryFilterSql = selectedIndustries
+    ? Prisma.sql`and CP."industry" in (${Prisma.join(selectedIndustries)})`
+    : Prisma.sql``;
+
+  const categoriesFilterSql = selectedCategories
+    ? Prisma.sql`and CAT."name" in (${Prisma.join(selectedCategories)})`
+    : Prisma.sql``;
+
+  const userEmailsFilterSql = selectedUserEmails
+    ? Prisma.sql`and U."email" in (${Prisma.join(selectedUserEmails)})`
+    : Prisma.sql``;
+
+  const emailFilterSql = selectedEmail
+    ? Prisma.sql`and C.email ilike ${"%" + selectedEmail + "%"}`
+    : Prisma.sql``;
+
+  const websiteFilterSql = selectedWebsite
+    ? Prisma.sql`and C.email ilike ${"%" + selectedWebsite + "%"}`
+    : Prisma.sql``;
+
+  const createdAfterFilterSql = createdAfter
+    ? Prisma.sql`and C."createdAt" >= ${new Date(createdAfter)}`
+    : Prisma.sql``;
+
+  const sql = Prisma.sql`
+      select distinct on (C.email) C.*
+      from "Contact" C
+               inner join public."User" U on U.id = C."userId"
+               inner join public."PersonProfile" PP on C.email = PP.email and PP."linkedInUrl" is not null
+               inner join public."PersonExperience" PE on PP.id = PE."personProfileId"
+               inner join public."CompanyProfile" CP on CP."linkedInUrl" = PE."companyLinkedInUrl"
+               left join public."CompanyProfileCategory" CPC on CP.id = CPC."companyProfileId"
+               left join public."Category" CAT on CPC."categoryId" = CAT.id
+      where 1 = 1 ${cityFilterSql} ${stateFilterSql} ${jobTitleFilterSql} ${emailFilterSql} \
+            ${websiteFilterSql} ${industryFilterSql} ${categoriesFilterSql} ${userEmailsFilterSql} \
+            ${createdAfterFilterSql}
+        and C."userId" != ${user.id}
+      order by email ASC, "receivedCount" DESC
+      offset ${recordsToSkip} limit ${itemsPerPage};
+  `;
+
+  console.log(sql.text, sql.values);
+
+  const prospects = await prisma.$queryRaw<Contact[]>(sql);
+  console.log("prospects found: ", prospects.length);
+
+  return prospects;
+};
+
+export default getProspectsBasedOnFilters;
+
+if (require.main === module) {
+  (async () => {
+    const user = await prisma.user.findFirstOrThrow();
+    const paginationValues: PaginatedValues = {
+      currentPage: 1,
+      itemsPerPage: 10,
+      recordsToSkip: 0
+    }
+    const d = new Date(2024, 4, 16);
+    console.log(d.toISOString());
+    const filters: SelectedFilterValues = {
+      selectedStates: ['New York']
+    };
+
+    const prospects = await getProspectsBasedOnFilters(filters, paginationValues, user);
+    console.log("prospects: ", prospects);
+    process.exit(0);
+  })();
+}
