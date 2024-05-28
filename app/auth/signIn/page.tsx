75ce55848d3cde0afeec5051cac99a2438e4bdae
Mister Singh <mistersingh179@gmail.com>
2024-05-28 16:18:18 -0400

    using google image as log in button

--- app/auth/signIn/page.tsx
+++ app/auth/signIn/page.tsx
@@ -5,7 +5,9 @@ import { Button } from "@/components/ui/button";
 import { LogIn } from "lucide-react";
 import { useSearchParams } from "next/navigation";
 import { Input } from "@/components/ui/input";
-import { Suspense } from "react";
+import React, { Suspense } from "react";
+import googleImage from "../web_light_sq_ctn@1x.png";
+import Image from "next/image";
 
 const MySignInForm = () => {
   const searchParams = useSearchParams();
@@ -18,10 +20,22 @@ const MySignInForm = () => {
         name={"callbackUrl"}
         value={callbackUrl ?? undefined}
       />
-      <Button type={"submit"}>
-        <div className={"mr-2"}>Sign-in with google</div>
-        <LogIn size={"18px"} strokeWidth={"2px"} />
+      <Button type={"submit"} className={"m-0 p-0"}>
+        <Image
+          src={googleImage}
+          alt={"google logo"}
+          className="hover:brightness-90"
+        />
       </Button>
+
+      {/*<Button type={"submit"}>*/}
+      {/*  <img*/}
+      {/*    className="w-6 h-6"*/}
+      {/*    src="https://www.svgrepo.com/show/475656/google-color.svg"*/}
+      {/*    alt="Google logo"*/}
+      {/*  />*/}
+      {/*  <div className={"ml-2"}>Continue with Google</div>*/}
+      {/*</Button>*/}
     </form>
   );
 };
