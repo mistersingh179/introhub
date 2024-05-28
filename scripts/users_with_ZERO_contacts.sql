6b4b00c8566183009344ce1cd8bd3f53543c7597
Mister Singh <mistersingh179@gmail.com>
2024-05-28 11:55:25 -0400

    added preview to emails

--- scripts/users_with_ZERO_contacts.sql
+++ scripts/users_with_ZERO_contacts.sql
@@ -1,29 +1,30 @@
+select "User".email, count(C.id) as count
+from "User"
+         left join public."Contact" C on "User".id = C."userId"
+where "User".email not in
+      ('sandeep@introhub.net', 'rod@introhub.net', 'mistersingh179@gmail.com', 'jrftbzcarcalendar@gmail.com',
+       'rod@brandweaver-email.com')
+group by "User".email
+having count(C.id) > 0;
+
 -- users with ZERO (0) contacts!!
 with foo as (select "User".email, count(C.id) as count
              from "User"
                       left join public."Contact" C on "User".id = C."userId"
              group by "User".email
              having count(C.id) = 0)
-select "User".id, "User".email, "User"."createdAt", foo.count, A.scope
+select "User".id, "User".email, A.scope, "User"."createdAt", foo.count
 from "User"
          inner join "foo" on "User".email = foo.email
          inner join public."Account" A on "User".id = A."userId"
 order by "User"."createdAt" DESC;
 
-select count(*)
-from "Contact"
-         inner join public."PersonProfile" PP on "Contact".email = PP.email
-where PP."linkedInUrl" is not null;
 
 select count(*)
 from "Contact";
 
-select *
-from "User"
-         inner join public."Account" A on "User".id = A."userId";
-
-select "User".email, count(C.id)
-from "User"
-         left join public."Message" C on "User".id = C."userId"
-group by "User".email
+select count(*)
+from "Contact"
+         inner join public."PersonProfile" PP on "Contact".email = PP.email
+where PP."linkedInUrl" is not null;
 
