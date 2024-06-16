420c01079b0d63a38bc0f9e6afa1c8495c033222
Mister Singh <mistersingh179@gmail.com>
2024-06-16 16:32:13 -0400

    adding microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import type { Viewport } from 'next'
+import type { Viewport } from "next";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,13 +13,13 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
   // Also supported by less commonly used
   // interactiveWidget: 'resizes-visual',
-}
+};
 
 export default function RootLayout({
   children,
@@ -27,6 +28,13 @@ export default function RootLayout({
 }>) {
   return (
     <html>
+      <Script id={"clarity"} strategy={"afterInteractive"}>
+        {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "mt0cpz8e8x");`}
+      </Script>
       <body className={`${inter.className}`}>
         <ThemeProvider
           attribute="class"
