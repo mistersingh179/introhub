f9c25cb324e3c52c0e3c519a9b7c2ca58832e811
Mister Singh <mistersingh179@gmail.com>
2024-06-17 09:59:41 -0400

    opened navigation for all

--- app/dashboard/NavigationItems.tsx
+++ app/dashboard/NavigationItems.tsx
@@ -1,9 +1,7 @@
-import ShowChildren from "@/components/ShowChildren";
 import NavLink from "@/components/NavLink";
 import { ModeToggle } from "@/components/MyToggleButton";
 import UserProfileImageNav from "@/app/dashboard/UserProfileImageNav";
-import React, { Dispatch, SetStateAction } from "react";
-import { SheetClose } from "@/components/ui/sheet";
+import React from "react";
 import { auth } from "@/auth";
 import { Session } from "next-auth";
 import prisma from "@/prismaClient";
@@ -21,61 +19,36 @@ export default async function NavigationItems({
       email: session.user?.email ?? "",
     },
   });
-  const allowedEmails = [
-    "sandeep@introhub.net",
-    "sandeep@brandweaver.ai",
-    "mistersingh179@gmail.com",
-    "joserodrigofuentes@gmail.com",
-    "rod@introhub.net",
-    "rod@brandweaver.ai",
-    "yaraarryn.464392@gmail.com",
-  ];
-  let userAllowed = false;
-  if (allowedEmails.includes(user.email!)) {
-    userAllowed = true;
-  }
   return (
     <>
       <div className={"mr-4 font-bold text-purple-600"}>IntroHub</div>
       <NavLink inSheet={inSheet} name={"Home"} url={"/dashboard/home"} />
-      <ShowChildren showIt={userAllowed}>
-        <>
-          {/*<NavLink*/}
-          {/*  inSheet={inSheet}*/}
-          {/*  name={"My Emails"}*/}
-          {/*  url={"/dashboard/my-emails"}*/}
-          {/*/>*/}
-          <NavLink
-            inSheet={inSheet}
-            name={"My Contacts"}
-            url={"/dashboard/my-contacts"}
-          />
-          <NavLink
-            inSheet={inSheet}
-            name={"Prospects"}
-            url={"/dashboard/prospects"}
-          />
-        </>
-      </ShowChildren>
+      {/*<NavLink*/}
+      {/*  inSheet={inSheet}*/}
+      {/*  name={"My Emails"}*/}
+      {/*  url={"/dashboard/my-emails"}*/}
+      {/*/>*/}
+      <NavLink
+        inSheet={inSheet}
+        name={"My Contacts"}
+        url={"/dashboard/my-contacts"}
+      />
+      <NavLink
+        inSheet={inSheet}
+        name={"Prospects"}
+        url={"/dashboard/prospects"}
+      />
       <NavLink
         inSheet={inSheet}
         name={"Introductions"}
         url={"/dashboard/introductions/list"}
       />
-      <ShowChildren showIt={userAllowed}>
-        <>
-          <NavLink
-            inSheet={inSheet}
-            name={"Settings"}
-            url={"/dashboard/settings"}
-          />
-          <NavLink
-            inSheet={inSheet}
-            name={"Profile"}
-            url={"/dashboard/profile"}
-          />
-        </>
-      </ShowChildren>
+      <NavLink
+        inSheet={inSheet}
+        name={"Settings"}
+        url={"/dashboard/settings"}
+      />
+      <NavLink inSheet={inSheet} name={"Profile"} url={"/dashboard/profile"} />
       <div className={"grow"}></div>
       <ModeToggle />
       <UserProfileImageNav />
