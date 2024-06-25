c7d8269c5af0d0b83a9707d01ccf37738595341e
Mister Singh <mistersingh179@gmail.com>
2024-06-25 11:15:47 -0400

    showing profiles of users on super page

--- app/dashboard/super/page.tsx
+++ app/dashboard/super/page.tsx
@@ -10,6 +10,12 @@ import {
 } from "@/components/ui/table";
 import AddCreditsDialog from "@/app/dashboard/super/AddCreditsDialog";
 import ImpersonateForm from "@/app/dashboard/super/ImpersonateForm";
+import FacilitatorBox from "@/components/FacilitatorBox";
+import getEmailAndCompanyUrlProfiles from "@/services/getEmailAndCompanyUrlProfiles";
+import getProfiles from "@/services/getProfiles";
+import { CompanyBox } from "@/app/dashboard/introductions/list/IntroTable";
+import * as React from "react";
+import ShowChildren from "@/components/ShowChildren";
 
 const Super = async () => {
   const allUsers = await prisma.user.findMany({
@@ -24,14 +30,18 @@ const Super = async () => {
     },
   });
 
+  const { emailToProfile, companyUrlToProfile } =
+    await getEmailAndCompanyUrlProfiles(allUsers.map((u) => u.email!));
+
   return (
     <>
       <h1 className={"text-2xl my-8"}>Super</h1>
-      <Table className={"table-fixed"}>
+      <Table className={"table-auto"}>
         <TableCaption>All Users</TableCaption>
         <TableHeader>
           <TableRow>
             <TableHead>id / email</TableHead>
+            <TableHead>Profile</TableHead>
             <TableHead>credits</TableHead>
             <TableHead>contacts</TableHead>
             <TableHead>facilitor</TableHead>
@@ -40,13 +50,36 @@ const Super = async () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {allUsers.map((user) => {
+          {allUsers.map(async (user) => {
+            const { personExp, companyProfile, personProfile } = getProfiles(
+              user.email!,
+              emailToProfile,
+              companyUrlToProfile,
+            );
+
             return (
               <TableRow key={user.id}>
                 <TableCell>
                   <div className={"flex flex-col gap-4"}>
-                    <div>{user.id}</div>
                     <div>{user.email}</div>
+                    <div>{user.id}</div>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <div className={"flex flex-col gap-4"}>
+                    <ShowChildren showIt={!!personExp}>
+                      <div>
+                        <FacilitatorBox user={user} personExp={personExp} />
+                      </div>
+                    </ShowChildren>
+                    <ShowChildren showIt={!!companyProfile && !!personExp}>
+                      <div>
+                        <CompanyBox
+                          companyProfile={companyProfile}
+                          personExp={personExp}
+                        />
+                      </div>
+                    </ShowChildren>
                   </div>
                 </TableCell>
                 <TableCell>{user.credits}</TableCell>
