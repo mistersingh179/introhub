e117b2a59736fa630d3a9b9e108d3a8e0ae58d45
Mister Singh <mistersingh179@gmail.com>
2024-08-05 14:58:02 -0400

    intro can now change from rejected to approved

--- services/process/processUserForAutoProspecting.ts
+++ services/process/processUserForAutoProspecting.ts
@@ -49,7 +49,7 @@ if (require.main === module) {
   (async () => {
     const user = await prisma.user.findFirstOrThrow({
       where: {
-        email: "ashnoorarneja@gmail.com",
+        email: "sandeep@introhub.net",
       },
     });
     const ans = await processUserForAutoProspecting(user);
