542687c6f0a54ccae85e2574d8af791e55ee295a
Mister Singh <mistersingh179@gmail.com>
2024-08-12 12:22:12 -0400

    fixing build by adding espace for apostraphe and adding missing imports

--- app/dashboard/AutoProspectingDialog.tsx
+++ app/dashboard/AutoProspectingDialog.tsx
@@ -31,57 +31,114 @@ const AutoProspectingDialog = (props: AutoProspectingDialogProps) => {
       <AlertDialog open={open} onOpenChange={setOpen}>
         <AlertDialogContent className={"w-full max-w-4xl"}>
           <AlertDialogHeader>
-            <AlertDialogTitle>
-              Welcome to IntroHub 2.0 🎉 {" "}
-            </AlertDialogTitle>
+            <AlertDialogTitle>Welcome to IntroHub 2.0 🎉 </AlertDialogTitle>
             <AlertDialogDescription>
               <div>
-                We aim to get you more meetings per month with target prospects. 
-                To accomplish this goal, we automated intro requests and 
-                approvals. Here's what you need to know.
+                We aim to get you more meetings per month with target prospects.
+                To accomplish this goal, we automated intro requests and
+                approvals. {"Here's"} what you need to know.
               </div>
               <h2>Introduction Requests</h2>
               <div className="mt-4">
                 <ul>
-                  <li style={{ listStyle: 'outside', marginLeft: '1em', marginBottom: '1em' }}>
-                    We automatically handle prospecting for you based on your Ideal Customer Profile (ICP). Set your ICP by saving filters and starring profiles on the Prospects page.
+                  <li
+                    style={{
+                      listStyle: "outside",
+                      marginLeft: "1em",
+                      marginBottom: "1em",
+                    }}
+                  >
+                    We automatically handle prospecting for you based on your
+                    Ideal Customer Profile (ICP). Set your ICP by saving filters
+                    and starring profiles on the Prospects page.
                   </li>
-                  <li style={{ listStyle: 'outside', marginLeft: '1em', marginBottom: '1em' }}>
-                    Each day, we'll request an intro to prospects that match your ICP.
+                  <li
+                    style={{
+                      listStyle: "outside",
+                      marginLeft: "1em",
+                      marginBottom: "1em",
+                    }}
+                  >
+                    Each day, {"we'll"} request an intro to prospects that match
+                    your ICP.
                   </li>
-                  <li style={{ listStyle: 'outside', marginLeft: '1em', marginBottom: '1em' }}>
-                    If a prospect consents, then you'll be CC'd on an intro email so you can forge a relationship.
+                  <li
+                    style={{
+                      listStyle: "outside",
+                      marginLeft: "1em",
+                      marginBottom: "1em",
+                    }}
+                  >
+                    If a prospect consents, then {"you'll"} be {"CC'd"} on an
+                    intro email so you can forge a relationship.
                   </li>
                 </ul>
               </div>
               <h2>Facilitating Introductions</h2>
               <div className="mt-4">
                 <ul>
-                  <li style={{ listStyle: 'outside', marginLeft: '1em', marginBottom: '1em' }}>
-                    When one of your contacts matches a requester’s ICP, we’ll send an email on your behalf to ask if they're open to an introduction.
+                  <li
+                    style={{
+                      listStyle: "outside",
+                      marginLeft: "1em",
+                      marginBottom: "1em",
+                    }}
+                  >
+                    When one of your contacts matches a requester’s ICP, we’ll
+                    send an email on your behalf to ask if {"they're"} open to an
+                    introduction.
                   </li>
-                  <li style={{ listStyle: 'outside', marginLeft: '1em', marginBottom: '1em' }}>
-                    Only with their explicit approval will we facilitate the connection to ensure respect and privacy.
+                  <li
+                    style={{
+                      listStyle: "outside",
+                      marginLeft: "1em",
+                      marginBottom: "1em",
+                    }}
+                  >
+                    Only with their explicit approval will we facilitate the
+                    connection to ensure respect and privacy.
                   </li>
-                  <li style={{ listStyle: 'outside', marginLeft: '1em', marginBottom: '1em' }}>
-                    You're in control. Make any contact 'Unavailable' for intros in the Contacts page.
+                  <li
+                    style={{
+                      listStyle: "outside",
+                      marginLeft: "1em",
+                      marginBottom: "1em",
+                    }}
+                  >
+                    {"You're"} in control. Make any contact {"'Unavailable'"} for intros
+                    in the Contacts page.
                   </li>
                 </ul>
               </div>
               <h2>Safeguarding Your Reputation</h2>
               <div className="mt-4">
                 <ul>
-                  <li style={{ listStyle: 'outside', marginLeft: '1em', marginBottom: '1em' }}>
-                    To prevent contact fatigue, we limit emails to one per person every 7 days.
+                  <li
+                    style={{
+                      listStyle: "outside",
+                      marginLeft: "1em",
+                      marginBottom: "1em",
+                    }}
+                  >
+                    To prevent contact fatigue, we limit emails to one per
+                    person every 7 days.
                   </li>
-                  <li style={{ listStyle: 'outside', marginLeft: '1em', marginBottom: '1em' }}>
-                    Your account will never send more than two intro requests per day.
+                  <li
+                    style={{
+                      listStyle: "outside",
+                      marginLeft: "1em",
+                      marginBottom: "1em",
+                    }}
+                  >
+                    Your account will never send more than two intro requests
+                    per day.
                   </li>
                 </ul>
-              </div>      
+              </div>
               <div className="mt-4">
-                Ready to get started? Opt in now and begin connecting with prospects!
-              </div>              
+                Ready to get started? Opt in now and begin connecting with
+                prospects!
+              </div>
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
