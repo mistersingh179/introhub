49c19fee9e692e3f5b03e5c769bc1df217bee5f5
Mister Singh <mistersingh179@gmail.com>
2024-08-27 13:41:59 -0400

    giving rod 3 extra intros

--- scripts/repl.ts
+++ scripts/repl.ts
@@ -1,19 +1,4 @@
 import prisma from "../prismaClient";
-import {
-  addDays,
-  addHours,
-  addMinutes,
-  formatDistance,
-  formatDistanceStrict,
-  startOfDay,
-  startOfHour,
-  startOfMinute,
-} from "date-fns";
-import { UTCDate } from "@date-fns/utc";
-import apolloQueue from "@/bull/queues/apolloQueue";
-import apolloWorker from "@/bull/workers/apolloWorker";
-import peopleEnrichmentApiCall from "@/services/helpers/apollo/peopleEnrichmentApiCall";
-import { CronJob } from "cron";
 
 // @ts-ignore
 prisma.$on("query", (e) => {
@@ -25,42 +10,12 @@ prisma.$on("query", (e) => {
 });
 
 (async () => {
-  const job = new CronJob(
-    "* * * * * *", // cronTime
-    async function () {
-        try{
-          // const ans = await peopleEnrichmentApiCall("sandeep@introhub.net");
-          console.log(new Date());
-        }catch(err){}
-    }, // onTick
-    null, // onComplete
-    true, // start
-    "America/New_York", // timeZone
-  );
 
-  // setInterval(async () => {
-  //   try{
-  //     const ans = await peopleEnrichmentApiCall("sandeep@introhub.net");
-  //   }catch(err){}
-  // }, )
+  Array.from({ length: 2 }).map(async (x) => {
+    console.log("In here");
+  });
 
-  return;
 
-  // const now = new UTCDate();
-  // const nextDay = startOfDay(addDays(now, 1));
-  // const waitTime = nextDay.getTime() - now.getTime()
-  // console.log(waitTime, waitTime/1000/60/60);
-
-  console.log(apolloWorker.isPaused());
-  console.log(apolloWorker.isRunning());
-
-  // apolloWorker.resume();
-
-  // await apolloWorker.rateLimit(1)
-  const ttlMs = await apolloQueue.getRateLimitTtl();
-  const now = new Date();
-  console.log("seconds: ", Math.round(ttlMs / 1000));
-  console.log("minutes: ", Math.round(ttlMs / 1000 / 60));
 })();
 
 export {};
