769003a49e64de8394b48a42d5cf09958eaaf90f
Mister Singh <mistersingh179@gmail.com>
2024-09-12 13:16:04 -0400

    updated migration file to not do subject index

--- prisma/migrations/20240912154324_adding_index_on_messages_for_subject_and_thread_id/migration.sql
+++ prisma/migrations/20240912154324_adding_index_on_messages_for_subject_and_thread_id/migration.sql
@@ -2,7 +2,4 @@
 ALTER TABLE "User" ALTER COLUMN "agreedToAutoProspecting" SET DEFAULT false;
 
 -- CreateIndex
-CREATE INDEX "Message_subject_idx" ON "Message"("subject");
-
--- CreateIndex
 CREATE INDEX "Message_threadId_idx" ON "Message"("threadId");
