94b95e641d084a5d4f01833eda316b7efa6d63e1
Mister Singh <mistersingh179@gmail.com>
2024-09-13 11:35:46 -0400

    made threadId optional again

--- prisma/migrations/20240912204404_making_thread_id_required_with_blank_default/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `threadId` on table `Introduction` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Introduction" ALTER COLUMN "threadId" SET NOT NULL,
-ALTER COLUMN "threadId" SET DEFAULT '';
