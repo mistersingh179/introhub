a0a5d85e14694b7a85d792d6d6a8dff29f6a47f8
Mister Singh <mistersingh179@gmail.com>
2024-10-21 15:22:26 -0400

    removed sql migration which connected person exp and company profile.
    manually fetching company and putting in person experience when building json for description

--- prisma/migrations/20241017135434_connecting_company_profile_with_person_experience/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateIndex
-CREATE INDEX "PersonExperience_companyLinkedInUrl_idx" ON "PersonExperience"("companyLinkedInUrl");
-
--- AddForeignKey
-ALTER TABLE "PersonExperience" ADD CONSTRAINT "PersonExperience_companyLinkedInUrl_fkey" FOREIGN KEY ("companyLinkedInUrl") REFERENCES "CompanyProfile"("linkedInUrl") ON DELETE CASCADE ON UPDATE CASCADE;
