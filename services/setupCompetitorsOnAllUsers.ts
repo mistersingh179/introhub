fa63c035d437dd86d169c914fcfefa1f8e16da10
Mister Singh <mistersingh179@gmail.com>
2024-11-13 16:47:09 -0500

    refactored content on competitors page.
    it now shows tooltip with smaller badges.
    made it responsive.

--- services/setupCompetitorsOnAllUsers.ts
+++ services/setupCompetitorsOnAllUsers.ts
@@ -2,13 +2,15 @@ import prisma from "@/prismaClient";
 import setupCompetitorsOnUser from "@/services/setupCompetitorsOnUser";
 
 const setupCompetitorsOnAllUsers = async () => {
-  console.log("in setupCompetitorsOnAllUsers");
+  console.log("Starting setupCompetitorsOnAllUsers");
 
   const users = await prisma.user.findMany({});
 
   for (const user of users) {
     await setupCompetitorsOnUser(user);
   }
+
+  console.log("Finishing setupCompetitorsOnAllUsers");
 };
 
 export default setupCompetitorsOnAllUsers;
